7a5cb97f22a369f999c5fd866eab4db7221db441
Radhika Bhatt <rbhatt@doc.gov>
2016-11-01 15:12:16 -0400

    created custom js file

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+// $('html').click(function() {
+//    $('.tooltip-function').hide(); 
+// });
+
+// $('.tooltip-function').click(function(event){
+//      event.stopPropagation();
+// });
+
+// $('#menutoggle').click(function(event){
+//      $('#menucontainer').toggle();
+// });
+
+// $(document).click(function(event) { 
+//     if(!$(event.target).closest('.custom-tooltip').length) {
+//         if($('.custom-tooltip').is(":in")) {
+//             $('.custom-tooltip').hide();
+//         }
+//     }        
+// });
+
+$('.tooltip-inner').on('click', function () {
+  $(this.hash).toggleClass('in').focus();
+});
+
+$('a').on('focusout', function () {
+  $(this).removeClass('in');
+});
